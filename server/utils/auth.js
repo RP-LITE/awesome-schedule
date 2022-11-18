5ab8be717aa79fe6206b2a278e470505e347e641
TGOLD <thomaswgold90@gmail.com>
2022-11-18 15:54:05 -0700

    changed user-routes to include accountType in token

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,9 +1,9 @@
 // Auth.js from REST version of challenge 21
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
 
 module.exports = {
   // function for our authenticated routes
@@ -13,11 +13,11 @@ module.exports = {
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return res.status(400).json({ message: "You have no token!" });
     }
 
     // verify token and get user data out of it
@@ -25,15 +25,15 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      console.log("Invalid token");
+      return res.status(400).json({ message: "invalid token!" });
     }
 
     // send to next endpoint
     next();
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  signToken: function ({ username, email, _id, accountType }) {
+    const payload = { username, email, _id, accountType };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
