b4142d1e2392c5485fec95c3d4cbb5ee9796511d
Alkria <adybass@gmail.com>
2022-11-27 19:00:54 -0700

    added class names and logo to home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,19 +6,21 @@ import SignUpForm from "../components/modals/SignUpForm";
 import Auth from "@/utils/Auth";
 import { Link } from "react-router-dom";
 import { UserContext } from "@/utils/UserContext";
+import Logo from "../assets/Jot it Down Logo.png";
 
 export default function HomePage() {
   const context = useContext(UserContext);
   return (
     <section className='page-container-home'>
-      <div className='flex m-0 flex-col content-center items-center py-10 px-20'>
-        <h1 className='text-6xl text-black'>Welcome to Awesome Schedule!</h1>
+       <div className='flex m-0 flex-col content-center items-center py-10 px-20'>
+       <img className='logo' src={Logo} alt='Jot it Down'></img>
+        
         <span className='text-3xl text-red'>
           ~ The Last Scheduler You'll Ever Need ~
         </span>
         <div className='content-center items-center py-10 px-20'>
           <p>
-            Awesome Schedule is a two way client-provider scheduling service.
+            Jot it Down is a two way client-provider scheduling service.
             Clients can search for providers and schedule appointments for
             services. Providers can post there available serices and service
             details, as well as hours of operation.
