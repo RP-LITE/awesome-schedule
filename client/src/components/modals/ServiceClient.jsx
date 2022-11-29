ad6c5ff6e15b3a6c3fb6fa04dc3225bb0f0db50c
TGOLD <thomaswgold90@gmail.com>
2022-11-29 16:23:00 -0700

    some final styling: modals, homepage, provider page

--- client/src/components/modals/ServiceClient.jsx
+++ client/src/components/modals/ServiceClient.jsx
@@ -7,16 +7,18 @@ export default function ServiceClient({ service }) {
   const context = useContext(UserContext);
 
   return (
-    <div className='providerServices'>
-      <h3>{context.provider.user.username}</h3>
-      <ul>
+    <div className='providerServices flex flex-col justify-center items-center'>
+      <h3 className='text-2xl mb-6 underline w-full text-center'>
+        {context.provider.user.username}
+      </h3>
+      <ul className='bg-red rounded sidebarlink sidebarlink2 w-full'>
         {context?.provider?.services &&
           context.provider.services.map((service) => {
             return (
               <li key={service._id}>
                 <ModalTemp
                   title={service.name}
-                  className='sidebarlink bg-red sidebarlink2 py-2 my-2'
+                  className='sidebarlink sidebarlink2 py-2 my-2'
                   modalTitle={service.name}
                 >
                   <ServiceInfoModal
--- client/src/components/modals/ServiceInfoModal.jsx
+++ client/src/components/modals/ServiceInfoModal.jsx
@@ -5,47 +5,51 @@ import ModalTemp from "./ModalTemplate";
 import { UserContext } from "@/utils/UserContext";
 
 const dateTimeConvert = (d) => {
-  const localD = new Date(Math.round(
-    (d.getTime() - d.getTimezoneOffset() * 60000
-    ) / 60 / 1000) * 60 * 1000);
+  const localD = new Date(
+    Math.round((d.getTime() - d.getTimezoneOffset() * 60000) / 60 / 1000) *
+      60 *
+      1000
+  );
   return localD.toISOString().slice(0, -1);
-}
+};
 const ServiceInfoModal = ({ service, provider }) => {
   const context = useContext(UserContext);
-  const [ now, setNow ] = useState(dateTimeConvert(new Date()));
+  const [now, setNow] = useState(dateTimeConvert(new Date()));
   const [userFormData, setUserFormData] = useState({
     start: "",
   });
 
   const handleScheduleAppt = (e) => {
     e.preventDefault();
-    const epochStart = (new Date(userFormData.start)).getTime()  / 60 / 1000;//Convert time to epoch minutes
-    context.scheduleAppt(context,{_id:service._id,start:epochStart});
+    const epochStart = new Date(userFormData.start).getTime() / 60 / 1000; //Convert time to epoch minutes
+    context.scheduleAppt(context, { _id: service._id, start: epochStart });
   };
-  
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
-  useEffect(()=>{
+  useEffect(() => {
     const intervalID = setInterval(
-      ()=>setNow(dateTimeConvert(new Date())),
-      15 * 60 * 1000//15 minutes
-    )
+      () => setNow(dateTimeConvert(new Date())),
+      15 * 60 * 1000 //15 minutes
+    );
     return () => clearInterval(intervalID);
-  },[])
+  }, []);
   return (
-    <form className='service-info'>
-      <h1 className='service-name'>{service.name}</h1>
-      <ul className='service-info'>
-        <li>Location:{provider.address}</li>
-        <li>Cost: ${service.cost}</li>
-        <li>Duration: {service.duration} minutes</li>
-        <li>Description: {service.description}</li>
+    <form className='service-info flex flex-col'>
+      <h1 className='service-name text-center text-2xl mb-4'>{service.name}</h1>
+      <ul className='service-info grid grid-cols-2 p-2 m-2 text-center text-sm'>
+        <li className='px-2 py-1 border-b'>Location:{provider.address}</li>
+        <li className='px-2 py-1 border-l border-b'>Cost: ${service.cost}</li>
+        <li className='px-2 py-1'>Duration: {service.duration} minutes</li>
+        <li className='px-2 py-1 border-l'>
+          Description: {service.description}
+        </li>
       </ul>
       <div className='labelInput'>
         <label className='label start' htmlFor='start'>
-          When
+          When:
         </label>
         <input
           min={now}
@@ -59,8 +63,13 @@ const ServiceInfoModal = ({ service, provider }) => {
           required
         ></input>
       </div>
-      <div className='action-buttons'>
-        <button onClick={handleScheduleAppt}>Schedule</button>
+      <div className='action-buttons flex justify-center items-center'>
+        <button
+          className='sidebarlink bg-red sidebarlink2 mt-4 text-lg'
+          onClick={handleScheduleAppt}
+        >
+          Schedule
+        </button>
       </div>
     </form>
   );
