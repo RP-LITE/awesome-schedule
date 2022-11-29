f42ce98dff0f048e3f185891c4e8bafc20e91562
Scott Casey <scaseydv@gmail.com>
2022-11-29 18:07:41 -0500

    Checking dist build

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const path = require('path');
 const apiRoutes = require('./api');
+const fs = require('fs/promises');
 
 router.use('/api', apiRoutes);
-
+(async () => {
+  const index = await fs.readFile('../../client/dist/index.html','utf8');
+  console.log('index',index);
+})();
 // serve up react front-end in production
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
