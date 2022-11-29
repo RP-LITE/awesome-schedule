c191cacb6360f4d139d892712b29ca15c136a055
Scott Casey <scaseydv@gmail.com>
2022-11-29 17:25:52 -0500

    Update procfile

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/awesomeschedule', {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/awesomeschedule', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
