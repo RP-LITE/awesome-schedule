ca1fafb21b0c0f2c8f3535134c9f2c38664e722d
Scott Casey <scaseydv@gmail.com>
2022-11-29 18:11:56 -0500

    Fix index log path

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,7 @@ const fs = require('fs/promises');
 router.use('/api', apiRoutes);
 console.log('__dirname',__dirname);
 (async () => {
-  const index = await fs.readFile('../../client/dist/index.html','utf8');
+  const index = await fs.readFile(path.join(__dirname, '../../client/dist/index.html'),'utf8');
   console.log('index',index);
 })();
 // serve up react front-end in production
